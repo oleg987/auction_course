a1f6d542798aa79129b08de6cb66128ebdd52ca0
Ярослав <93127105+Yaroslavlazarenko@users.noreply.github.com>
2025-03-13 17:03:57 +0200

    add user controller and service

--- SothbeysKillerApi/Controllers/UserController.cs
+++ SothbeysKillerApi/Controllers/UserController.cs
@@ -1,6 +1,55 @@
-﻿namespace SothbeysKillerApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using SothbeysKillerApi.Services;
 
-public class UserController
+namespace SothbeysKillerApi.Controllers;
+
+public record RegisterUserRequest(string Name, string Email, string Password);
+public record LoginUserRequest(string Email, string Password);
+public record LoginUserResponse(Guid Id, string Name, string Email);
+public class User
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService = new UserService();
+    
+    [HttpPost]
+    public IActionResult Register(RegisterUserRequest request)
+    {
+        try
+        {
+            _userService.RegisterUser(request);
+            return NoContent();
+        }
+        catch (ArgumentException)
+        {
+            return BadRequest();
+        }
+    }
+    
+    [HttpPost]
+    public IActionResult Login(LoginUserRequest request)
+    {
+        try
+        {
+            var response = _userService.LoginUser(request);
+            return Ok(response);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Unauthorized();
+        }
+        catch (ArgumentException)
+        {
+            return BadRequest();
+        }
+    }
     
 }
--- SothbeysKillerApi/Services/UserService.cs
+++ SothbeysKillerApi/Services/UserService.cs
@@ -1,6 +1,62 @@
-﻿namespace SothbeysKillerApi.Services;
+﻿using SothbeysKillerApi.Controllers;
 
-public class UserService
+namespace SothbeysKillerApi.Services;
+
+public interface IUserService
 {
+    void RegisterUser(RegisterUserRequest request);
+    LoginUserResponse LoginUser(LoginUserRequest request);
+}
+
+public class UserService : IUserService
+{
+    public static List<User> usersStorage = [];
     
+    public void RegisterUser(RegisterUserRequest request)
+    {
+        if (request.Email.Length < 3 || request.Email.Length > 255)
+        {
+            throw new ArgumentException("Email must be between 3 and 255 characters");
+        }
+
+        if (request.Name.Length < 3 || request.Name.Length > 255)
+        {
+            throw new ArgumentException("Name must be between 3 and 255 characters");
+        }
+
+        if (request.Password.Length < 3 || request.Password.Length > 255)
+        {
+            throw new ArgumentException("Password must be between 3 and 255 characters");
+        }
+        
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            Email = request.Email,
+            Name = request.Name,
+            Password = request.Password
+        };
+        
+        usersStorage.Add(user);
+    }
+    
+    public LoginUserResponse LoginUser(LoginUserRequest request)
+    {
+        if (usersStorage.All(u => u.Email != request.Email))
+        {
+            throw new UnauthorizedAccessException();
+        }
+        
+        var user = usersStorage
+            .Where(u => u.Email == request.Email && u.Password == request.Password)
+            .Select(user => new LoginUserResponse(user.Id, user.Name, user.Email))
+            .FirstOrDefault();
+
+        if (user is null)
+        {
+            throw new ArgumentException("Invalid password");
+        }
+        
+        return user;
+    }
 }
